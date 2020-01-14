05d13f1d6526ab442aa6ee420125eccf18177ef0
grishnan <grishnan@gmail.com>
2020-01-14 11:24:14 +0300

    Adding collision detection. Add class Sled

--- 1.py
+++ 1.py
@@ -2,24 +2,25 @@
 
 import pygame as pg
 from pygame.locals import *
-from go import Ball # игровые объекты
+from go import Ball, Sled # игровые объекты
 
 pg.init()
 
 FPS  = 60
 SIZE = width, height = 640, 480
-speedsled = [0, 0]
 BGCOLOR = 90, 90, 90
 
 screen = pg.display.set_mode(SIZE)         # создаём окошко
 clock  = pg.time.Clock()                   # создаем таймер
 
-sled = pg.image.load("sled.png").convert() # загрузка изображения платформы
-sledrect = sled.get_rect()                 # снимаем прямоугольник для платформы
-sledrect.move_ip((20, 0))                  # поместить прямоугольник платформы в указанное место
-sledmask = pg.mask.from_surface(sled)      # снимаем маску с платформы
+ball1 = Ball("ball.png", (50, 20), [4, 4])   # создаём мяч
+ball2 = Ball("ball.png", (50, 20), [-4, 3])  # создаём мяч
 
-ball = Ball("ball.png", (50, 20), [4, 4])  # создаём мяч
+sled = Sled("sled.png", (20, 0), [0, 0]) # создаём платформу
+
+balls = pg.sprite.Group()
+balls.add(ball1)
+balls.add(ball2)
 
 again = True
 while again:
@@ -29,28 +30,36 @@ while again:
     # логика перемещения платформы
     pressed = pg.key.get_pressed() # вернем все зажатые клавиши
     if pressed[pg.K_UP]:
-        speedsled[1] = -2 if sledrect.top >= 0 else 0
+        sled.speed[1] = -2 if sled.rect.top >= 0 else 0
     elif pressed[pg.K_DOWN]:
-        speedsled[1] = 2 if sledrect.bottom <= height else 0
-    else: speedsled[1] = 0
+        sled.speed[1] = 2 if sled.rect.bottom <= height else 0
+    else: sled.speed[1] = 0
 
     # логика столкновения мяча с платформой
-    if sledrect.colliderect(ball.rect):
-        ball.speed[0] = -ball.speed[0]
+    if pg.sprite.spritecollide(sled, balls, False, pg.sprite.collide_mask):
+        for ball in balls: ball.speed[0] = -ball.speed[0]
 
     # логика отскока мяча от границ экрана
-    if ball.rect.left < 0 or ball.rect.right > width:
-        ball.speed[0] = -ball.speed[0]
-    if ball.rect.top < 0 or ball.rect.bottom > height:
-        ball.speed[1] = -ball.speed[1]
-        
-    sledrect = sledrect.move(speedsled)    # сдвинуть прямоугольник платформы
-    ball.rect = ball.rect.move(ball.speed) # сдвинуть прямоугольник мяча
-
+    if ball1.rect.left < 0 or ball1.rect.right > width:
+        ball1.speed[0] = -ball1.speed[0]
+    if ball1.rect.top < 0 or ball1.rect.bottom > height:
+        ball1.speed[1] = -ball1.speed[1]
+    
+    # логика отскока мяча от границ экрана
+    if ball2.rect.left < 0 or ball2.rect.right > width:
+        ball2.speed[0] = -ball2.speed[0]
+    if ball2.rect.top < 0 or ball2.rect.bottom > height:
+        ball2.speed[1] = -ball2.speed[1]
+    
+    sled.rect = sled.rect.move(sled.speed)    # сдвинуть прямоугольник платформы
+    ball1.rect = ball1.rect.move(ball1.speed) # сдвинуть прямоугольник мяча
+    ball2.rect = ball2.rect.move(ball2.speed) # сдвинуть прямоугольник мяча
+    
     # отрисовка изображений
     screen.fill(BGCOLOR)
-    screen.blit(sled, sledrect)
-    screen.blit(ball.img, ball.rect)
+    screen.blit(sled.img, sled.rect)
+    screen.blit(ball1.img, ball1.rect)
+    screen.blit(ball2.img, ball2.rect)
 
     pg.display.update()
     clock.tick(FPS)
